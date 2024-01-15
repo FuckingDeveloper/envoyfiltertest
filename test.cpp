54b46a19cafe657ecff0dc9ffe2a7c6ef1efb224
MRH <sdan02@yandex.ru>
2024-01-15 10:14:05 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,41 +1,15 @@
-#include <iostream>
+#include <vector>
 
-using namespace std;
-
-// Функция, проверяющая, является ли число делителем для всех чисел в массиве
-bool checkDivisor(const vector<int>& array, int num) {
-    for (int i : array) {
-        if (num % i != 0) {
-            return false;
+int countChocolatePieces(std::vector<int> chocolate, int M, int D) {
+    int count = 0;
+    for (int i = 0; i < chocolate.size(); i++) {
+        int sum = 0;
+        for (int j = i; j < i + M && j < chocolate.size(); j++) {
+            sum += chocolate[j];
+        }
+        if (sum == D) {
+            count++;
         }
     }
-    return true;
-}
-
-// Функция, проверяющая, делится ли число на все числа в массиве
-bool checkDivisible(const vector<int>& array, int num) {
-    for (int i : array) {
-        if (i % num != 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    vector<int> a = {2, 6};
-    vector<int> b = {24, 36};
-
-    // Найти максимум в a и минимум в b
-    int maxA = *max_element(a.begin(), a.end());
-    int minB = *min_element(b.begin(), b.end());
-
-    // Проверить все числа между maxA и minB
-    for (int i = maxA; i <= minB; ++i) {
-        if (checkDivisor(a, i) && checkDivisible(b, i)) {
-            std::cout << i << " ";
-        }
-    }
-
-    return 0;
+    return count;
 }
