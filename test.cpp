8bbda65b396017694649b359b190aeed51faefe0
MRH <sdan02@yandex.ru>
2024-01-16 16:06:36 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,15 +1,21 @@
-#include <vector>
+#include <iostream>
+#include <limits>
 
-int countChocolatePieces(std::vector<int> chocolate, int M, int D) {
-    int count = 0;
-    for (int i = 0; i < chocolate.size(); i++) {
-        int sum = 0;
-        for (int j = i; j < i + M && j < chocolate.size(); j++) {
-            sum += chocolate[j];
+int main() {
+    int size = 0;
+    std::cout << "Введите значение размера: ";
+    if(std::cin >> size) {
+        if(size > 0) {
+            std::cout << "Число является положительным целым числом." << std::endl;
+        } else {
+            std::cout << "Число не является положительным целым числом." << std::endl;
         }
-        if (sum == D) {
-            count++;
-        }
+    } else {
+        std::cout << "Введено не целое число." << std::endl;
+        // очищаем ошибки и игнорируем оставшийся ввод
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    return count;
+
+    return 0;
 }
